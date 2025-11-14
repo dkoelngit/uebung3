7321ae9f0ff6f94049c3e2a7d668dc26c11a3cbd
dkoelngit <dariushplays@yahoo.com>
2025-11-14 21:40:16 +0100

    mehrere Fälle ausprobiert

--- src/h1/h1_main.java
+++ src/h1/h1_main.java
@@ -1,6 +1,20 @@
+package h1;
+
 public class h1_main {
     public static void main(String[] args) {
-        
-    }
 
+        int[] myArray = {5, 9, 8, 8, 10, 100, 7, 10};
+        
+        for (int i = 0; i < myArray.length / 2; i++) {
+            int temp = myArray[i];
+            myArray[i] = myArray[myArray.length - 1 - i];
+            myArray[myArray.length - 1 - i] = temp;
+        }
+        
+        System.out.print("Umgekehrtes Array: ");
+        for (int value : myArray) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+    }
 }
